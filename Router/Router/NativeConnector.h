d940ec39f96638516b94171c5aa6ab6ac6eba46c
lili <lili2011050508@163.com>
2018-11-09 22:10:41 +0800

    优化了本地调用逻辑

--- Router/Router/NativeConnector.h
+++ Router/Router/NativeConnector.h
@@ -13,6 +13,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NativeConnector : NSObject<URLConnector>
 
+
+/**
+ 本地组件和接口调用
+
+ @param target 接口类名称，本地类名称
+ @param action 要调用的接口或者本地类的方法
+ @param parameter 方法参数 NSDictionary *
+ @param error NSError 错误对象
+ @return 返回action 的返回值
+ */
+- (id)performTarget:(NSString *)target withAction:(NSString *)action withParameter:(NSDictionary *)parameter error:(NSError **)error;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Router/Router/NativeConnector.m
+++ Router/Router/NativeConnector.m
@@ -15,35 +15,22 @@
 }
 
 - (id)performURL:(NSURL *)URLObject error:(NSError * _Nullable __autoreleasing *)error {
-    // 解析链接：类名、方法、参数
-    NSString *urlString = [URLObject.absoluteString copy];
-    // 去掉scheme
-    NSString *url = [[urlString componentsSeparatedByString:@"//"] lastObject];
-    // 判断是否有参数
-    NSRange r = [url rangeOfString:@"?"];
-    NSString *targetName = nil, *actionName = nil;
+    // 解析URL
+    NSURLComponents *components = [NSURLComponents componentsWithString:URLObject.absoluteString];
+    // 获取path（action）
+    NSString *path = [[components.path componentsSeparatedByString:@"/"] lastObject];
+    // 转换成Action
+    NSString *action = [path copy];
+    if (components.query.length) {
+        action = [action stringByAppendingString:@":"];
+    }
+    // 解析query（参数）
     NSMutableDictionary *params = [NSMutableDictionary new];
-    if (NSNotFound == r.location) { // 无参数
-        NSArray *components = [url componentsSeparatedByString:@"/"];
-        targetName = [components firstObject];
-        actionName = [components lastObject];
-    } else {
-        NSArray *components = [url componentsSeparatedByString:@"?"];
-        NSString *targetAction = [components firstObject];
-        NSString *paramsString = [components lastObject];
-        // 解析类名和方法名
-        NSArray *targetActionList = [targetAction componentsSeparatedByString:@"/"];
-        targetName = [targetActionList firstObject];
-        actionName = [[targetActionList lastObject] stringByAppendingString:@":"];
-        // 解析参数
-        NSArray *keyValues = [paramsString componentsSeparatedByString:@"&"];
-        for (NSString *keyValueString in keyValues) {
-            NSArray *keyValueList = [keyValueString componentsSeparatedByString:@"="];
-            [params setObject:[keyValueList lastObject] forKey:[keyValueList firstObject]];
-        }
+    for (NSURLQueryItem *item in components.queryItems) {
+        [params setObject:item.value forKey:item.name];
     }
     
-    return [self performTarget:[NSString stringWithFormat:@"Target_%@", targetName] withAction:actionName withParameter:[params copy] error:error];
+    return [self performTarget:[NSString stringWithFormat:@"Target_%@", components.host] withAction:action withParameter:[params copy] error:error];
 }
 
 - (id)performTarget:(NSString *)target withAction:(NSString *)action withParameter:(NSDictionary *)parameter error:(NSError * _Nullable __autoreleasing *)error {
